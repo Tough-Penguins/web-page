1d51ef7b11e8c79929b822166d82af750329b619
UlVc <ulvc@ciencias.unam.mx>
2021-09-05 20:07:36 -0500

    Timer

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -52,3 +52,35 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 5, 2021 20:00:00").getTime();
+var btn = document.getElementById("btnCounter");
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get today's date and time
+var now = new Date().getTime();
+
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Display the result in the element with id="demo"
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+// If the count down is finished, write some text
+if (distance < 0) {
+    clearInterval(x);
+    btn.style.visibility = "visible";
+    document.getElementById("text-counter").style.visibility = "hidden";
+    document.getElementById("text-counter2").style.visibility = "hidden";
+    document.getElementById("text-counter3").style.visibility = "hidden";
+    }
+}, 1000);
